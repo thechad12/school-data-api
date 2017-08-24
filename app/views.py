c57e2b2c527ad505f963d9b19f02557cafdebd61
Kevin Pasquarella <pasquarella4292@gmail.com>
2017-08-23 20:37:07 -0400

    update views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from app import app
+from app import app, db
+from models import School
+from flask import render_template, jsonify, redirect, session, url_for,\
+	request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
+
+@app.route('/schools/')
+def schoolsSerialize():
+	schools = School.query().all()
+	return jsonify(schools=[s.serialize for s in schools])
 
