e42db0855634c2ad89fd9e69c21b9e92750b1648
Kevin Pasquarella <pasquarella4292@gmail.com>
2018-01-26 12:30:33 -0500

    add decorators for required login functions

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app, db
 from models import School, User
 from flask import render_template, jsonify, redirect, session, url_for,\
 	request, make_response
+from flask_httpauth import HTTPBasicAuth as auth
 import json
 
 @app.route('/')
@@ -22,22 +23,34 @@ def new_account():
 	user = User(username=username)
 	db.session.add(user)
 	db.session.commit()
+	return render_template('index.html')
 
 # Return all schools in db
 @app.route('/schools/')
+@auth.login_required
 def schools_serialize():
 	schools = db.session.query(School).all()
 	return jsonify(schools=[s.serialize for s in schools])
 
 # Return information on specific school in db based on ID
 @app.route('/schools/<int:school_id>')
+@auth.login_required
 def return_school(school_id):
 	school_id = school_id
 	school = db.session.query(School).filter_by(id=school_id).one()
 	return jsonify(school=school.serialize)
 
+# Verify password
+@auth.verify_password
+def verify_password(username, password):
+	user = db.session.query(User).filter_by(username=username).one()
+	if not user or not user.verify_password(password):
+		return False
+	return True
+
 # Generate auth token for new user
 @app.route('/api/token')
+@auth.login_required
 def get_auth_token():
 	token = User.gen_auth_token()
 	return jsonify({'token': token.decode('ascii')})
